ef121632b538d41dbd2b03cdada442efe16652a4
Joseph Okefe <your.email@example.com>
2025-04-11 11:32:26 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/landingPage/header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      {/* Other components can be added here */}
+    </div>
+  );
+};
+
+export default App;
