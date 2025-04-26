e29a1721b731cd308fd0a6b5e5a22facdabb6239
TopBoy7 <okefejoseph9@gmail.com>
2025-04-25 22:35:49 -1200

    featureSectionUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/landingPage/header";
 import HeroSection from "./components/landingPage/heroSection";
+import FeatureSection from "./components/landingPage/FeatureSection";
 const App = () => {
   return (
     <div>
       <Header />
       <HeroSection />
+      <FeatureSection />
     </div>
   );
 };
---
+++ src/components/landingPage/FeatureSection.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./FeatureSection.module.css";
+
+function FeatureSection() {
+  return (
+    <div>
+      <div className={`${styles.container} ${styles.textcenter}`}>
+        <h2>For Individuals and Businesses</h2>
+        <p>Join 200+ businesses using Eazipay's easy solution.</p>
+      </div>
+
+      <div className={styles.cards}>
+        <div className={`${styles.greenCard} ${styles.card1}`}>
+          <h3>Tamper-proof Payroll for Life</h3>
+
+          <p>Your staff payroll history is kept in one place forever.</p>
+          <p>No more excel sheet or manual records.</p>
+          <p>Download your payroll history anytime you need it.</p>
+        </div>
+        <div className={`${styles.greenCard} ${styles.card2}`}>
+          <h3>All Payroll, Anytime Anywhere</h3>
+
+          <p>
+            Wherever you are, Eazipay has got you covered on ALL your Payroll
+            tasks.
+          </p>
+          <p>
+            Whether it is Taxes, Pension insurances HMOs, trustfunds, Eazipay
+            handle all your compliances in one place and easily, in seconds!
+          </p>
+        </div>
+        <div className={`${styles.greenCard} ${styles.card3}`}>
+          <h3>Payroll in Seconds</h3>
+
+          <p>Never again will you spend more than 2 minutes on payroll.</p>
+          <p>Just click on your staff and bulk-pay them at once.</p>
+          <p>Payment is done permanently.</p>
+        </div>
+      </div>
+
+      <div class={styles.query}>
+        <p class={styles.textcenter}>
+          We are happy to answer your queries. Please, reach us at
+          <a href="mailto:hi@eazypay.com" class="red">
+            hi@myeazipay.com{" "}
+          </a>
+          and expect our response shortly after.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default FeatureSection;
